8ba2ab6b84911b34d9faba20c1ecfb0d93f1c0bd
Daniel Black <danblack9988@gmail.com>
2023-08-03 11:52:29 +0100

    small bugfixes for deployment

--- mainsite-fe/src/app/layout.jsx
+++ mainsite-fe/src/app/layout.jsx
@@ -5,7 +5,8 @@ import '@/styles/tailwind.css'
 export const metadata = {
   title: {
     template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    default:
+      'Professional full stack developer and designer. I build beautiful, responsive websites and mobile applications.',
   },
 }
 
--- mainsite-fe/src/app/page.jsx
+++ mainsite-fe/src/app/page.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { ContactSection } from '@/components/ContactSection'
 import { Container } from '@/components/Container'
@@ -9,39 +8,60 @@ import { SectionIntro } from '@/components/SectionIntro'
 import { StylizedImage } from '@/components/StylizedImage'
 import { Testimonial } from '@/components/Testimonial'
 import imageLaptop from 'public/codeBG.jpg'
-import FadeInLetters  from '@/components/FadeInLetters'
+import FadeInLetters from '@/components/FadeInLetters'
 
 const proficiencies = [
-  {label: 'HTML 5', logo: "html.png"},
-  {label: 'CSS 3', logo: "CSS.png"},
-  {label: 'ES6', logo: "ES6.png"},
-  {label: 'TypeScript', logo: "typescript.png"},
-  {label: 'React', logo: "react.png"},
-  {label: 'Next JS', logo: "next.png"},
-  {label: 'Redux', logo: "redux.png"},
-  {label: 'Tailwind CSS', logo: "tailwind.png"},
-  {label: 'Sass', logo: "sass.png"},
-  {label: 'React Native', logo: "react.png"},
-  {label: 'Native Base', logo: "nativebase.png"},
-  {label: 'Expo', logo: "expo.png"},
-  {label: 'Node JS', logo: "node.png"},
-  {label: 'Adonis 5', logo: "adonis.png"},
-  {label: 'PostgreSQL', logo: "postgres.png"},
-  {label: 'MySQL', logo: "sql.png"},
-  {label: 'Docker', logo: "docker.png"},
-  {label: "NginX", logo: "nginx.png"},
-  {label: 'GIT', logo: "git.png"},
-  {label: 'Adobe Photoshop', logo: "photoshop.png"},
-  {label: 'Adobe Illustrator', logo: "illustrator.png"},
+  { label: 'HTML 5', logo: 'html.png' },
+  { label: 'CSS 3', logo: 'CSS.png' },
+  { label: 'ES6', logo: 'ES6.png' },
+  { label: 'TypeScript', logo: 'typescript.png' },
+  { label: 'React', logo: 'react.png' },
+  { label: 'Next JS', logo: 'next.png' },
+  { label: 'Redux', logo: 'redux.png' },
+  { label: 'Tailwind CSS', logo: 'tailwind.png' },
+  { label: 'Chakra UI', logo: 'chakra.png' },
+  { label: 'Sass', logo: 'sass.png' },
+  { label: 'React Native', logo: 'react.png' },
+  { label: 'Native Base', logo: 'nativebase.png' },
+  { label: 'Expo', logo: 'expo.png' },
+  { label: 'Node JS', logo: 'node.png' },
+  { label: 'Adonis 5', logo: 'adonis.png' },
+  { label: 'PostgreSQL', logo: 'postgres.png' },
+  { label: 'MySQL', logo: 'sql.png' },
+  { label: 'Docker', logo: 'docker.png' },
+  { label: 'NginX', logo: 'NginX.png' },
+  { label: 'GIT', logo: 'git.png' },
+  { label: 'Adobe Photoshop', logo: 'photoshop.png' },
+  { label: 'Adobe Illustrator', logo: 'illustrator.png' },
 ]
 
 const portfolioProjects = [
   {
-    href:`https://${process.env.NEXT_PUBLIC_DOMAIN}`,
+    href: `https://scrolladex.${process.env.NEXT_PUBLIC_DOMAIN}`,
     title: 'Scroll-A-Dex!',
     description: `Personnel management app`,
-    features: ["Authentication", "CRUD", "REST API", "File uploads", "Form validation", "Search", "Filtering", "Responsive"],
-    stack: ["React", "Next JS", "Redux", "Chakra UI", "Adonis 5", "Node JS", "Postgres", "Axios", "Fornik", "Yup"]
+    features: [
+      'Authentication',
+      'CRUD',
+      'REST API',
+      'File uploads',
+      'Form validation',
+      'Search',
+      'Filtering',
+      'Responsive',
+    ],
+    stack: [
+      'React',
+      'Next JS',
+      'Redux',
+      'Chakra UI',
+      'Adonis 5',
+      'Node JS',
+      'Postgres',
+      'Axios',
+      'Fornik',
+      'Yup',
+    ],
   },
 ]
 
@@ -60,13 +80,19 @@ function Clients() {
             role="list"
             className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-6"
           >
-            {proficiencies.map(({label, logo}, index) => (
+            {proficiencies.map(({ label, logo }, index) => (
               <li key={index}>
                 <FadeIn>
-                  <div className='flex flex-col jusify-center items-center'>
-                    <Image src={logo} alt={label} width={75} height={75} unoptimized />
-                    <p className="mt-6 flex  gap-x-2 text-md text-neutral-950 ">
-                    {label}
+                  <div className="jusify-center flex flex-col items-center">
+                    <Image
+                      src={logo}
+                      alt={label}
+                      width={75}
+                      height={75}
+                      unoptimized
+                    />
+                    <p className="text-md mt-6  flex gap-x-2 text-neutral-950 ">
+                      {label}
                     </p>
                   </div>
                 </FadeIn>
@@ -86,57 +112,57 @@ function Portfolio() {
         title="Portfolio projects"
         className="mt-24 sm:mt-32 lg:mt-40"
       >
-        <p>
-          More to come soon!
-        </p>
+        <p>More to come soon!</p>
       </SectionIntro>
       <Container className="mt-16 overflow-hidden">
         <FadeInStagger className="grid grid-cols-1 gap-8 ">
           {portfolioProjects.map((project) => (
             <FadeIn key={project.href} className="flex max-w-full">
-                <a target="_blank" rel="noopener noreferrer" href={project.href}>
-                <article className="relative flex justify-between min-h-[800px] md:min-h-[500px] flex-1 w-full flex-col rounded-3xl ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 ">
-                  <div className='bg-black rounded-t-3xl'>
-                    <div className='p-6 md:p-8 text-orange-500'>
+              <a target="_blank" rel="noopener noreferrer" href={project.href}>
+                <article className="relative flex min-h-[800px] w-full flex-1 flex-col justify-between rounded-3xl ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 md:min-h-[500px] ">
+                  <div className="rounded-t-3xl bg-black">
+                    <div className="p-6 text-orange-500 md:p-8">
                       <p className="font-display text-3xl font-semibold">
                         {project.title}
                       </p>
-                      <p className="mt-4 text-base">
-                        {project.description}
-                      </p>
+                      <p className="mt-4 text-base">{project.description}</p>
                     </div>
                   </div>
-                  <div className='absolute top-[128px] md:top-[144px] left-0 w-full h-full'>
+                  <div className="absolute left-0 top-[128px] h-full w-full md:top-[144px]">
                     <Image
-                      src='/scrolladex.png'
-                      alt='Background Image'
-                      layout='fill'
-                      objectFit='cover'
-                      objectPosition='center'
+                      src="/scrolladex.png"
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
                     />
                   </div>
-                  <div className='p-6 sm:p-8 relative z-10 bg-black bg-opacity-75'>
-                    <div className="flex flex-row gap-2 flex-wrap mt-2"> 
-                      {project.features && project.features.map((feature, index) => (
-                        <div key={index} className='text-xs bg-gray-500 text-white rounded-xl py-1 px-2'>
-                          <p>
-                            {feature}
-                          </p>
-                        </div>
-                      ))}
+                  <div className="relative z-10 bg-black bg-opacity-75 p-6 sm:p-8">
+                    <div className="mt-2 flex flex-row flex-wrap gap-2">
+                      {project.features &&
+                        project.features.map((feature, index) => (
+                          <div
+                            key={index}
+                            className="rounded-xl bg-gray-500 px-2 py-1 text-xs text-white"
+                          >
+                            <p>{feature}</p>
+                          </div>
+                        ))}
                     </div>
-                    <div className="flex flex-row gap-2 flex-wrap mt-2"> 
-                      {project.stack && project.stack.map((feature, index) => (
-                        <div key={index} className='text-xs bg-orange-500 text-white rounded-xl py-1 px-2'>
-                          <p>
-                            {feature}
-                          </p>
-                        </div>
-                      ))}
+                    <div className="mt-2 flex flex-row flex-wrap gap-2">
+                      {project.stack &&
+                        project.stack.map((feature, index) => (
+                          <div
+                            key={index}
+                            className="rounded-xl bg-orange-500 px-2 py-1 text-xs text-white"
+                          >
+                            <p>{feature}</p>
+                          </div>
+                        ))}
                     </div>
                   </div>
                 </article>
-                </a>
+              </a>
             </FadeIn>
           ))}
         </FadeInStagger>
@@ -152,8 +178,7 @@ function Services() {
         eyebrow="What am I all about?"
         title="Impactful designs, clean code, efficient deployment"
         className="mt-24 sm:mt-32 lg:mt-40"
-      >
-      </SectionIntro>
+      ></SectionIntro>
       <Container className="mt-16">
         <div className="lg:flex lg:items-center lg:justify-end">
           <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
@@ -167,16 +192,16 @@ function Services() {
           </div>
           <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
             <ListItem title="Frontend development">
-              {`Good front end development is not just about writing code. It's about putting yourself in the customers' shoes and wondering if they are thinking "Wow, this is amazing!". The bit that people see is the most important part of any project. Only pixel perfect will do.`} 
+              {`Good front end development is not just about writing code. It's about putting yourself in the customers' shoes and wondering if they are thinking "Wow, this is amazing!". The bit that people see is the most important part of any project. Only pixel perfect will do.`}
             </ListItem>
             <ListItem title="Backend development">
-              {`Backend development plays a critical role in the scalability of any web application or digital platform. A well-designed backend architecture ensures the platform can handle increased demand and user load without compromising on performance, reliability, or speed.`} 
+              {`In todays cloud computing world compute time is money and scalability is key. Fetching must be efficient and secure. I can build REST APIs using Node JS and Adonis 5, with PostgreSQL or MySQL databases.`}
             </ListItem>
             <ListItem title="App development">
               {`In today's world of mobile devices, having a mobile app is a must. Using React Native, I can build mobile apps which work across iOS and Android.`}
             </ListItem>
-            <ListItem title="Always learning!">
-              {`Up to date with the latest changes in the industry, I am up to speed with the latest changes to React, namely the introduction to server components and leveraging SSG using Next JS.`}
+            <ListItem title="Efficient Deployment">
+              {`Using layered docker builds, nginx reverse proxies, and CI/CD pipelines, I can ensure any application is deployed efficiently and securely, frontend, backend and database.`}
             </ListItem>
           </List>
         </div>
@@ -194,28 +219,25 @@ export default async function Home() {
   return (
     <>
       <Container className="mt-24 sm:mt-32 md:mt-56">
-      <h1 className="font-display text-5xl tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl weight">
-        <div className='flex flex-row space-x-4'>
-          <FadeInLetters className='font-thin' direction="down">DANIEL</FadeInLetters>
-          <FadeInLetters direction="up">BLACK</FadeInLetters>
-        </div>
-      </h1>
-      <FadeIn delay={0.6}>
-        <p className="mt-6 text-xl text-neutral-600">
-          Full stack developer, designer, and general clever clogs.
-        </p>
-      </FadeIn>
-
+        <h1 className="weight font-display text-5xl tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+          <div className="flex flex-row space-x-4">
+            <FadeInLetters className="font-thin" direction="down">
+              DANIEL
+            </FadeInLetters>
+            <FadeInLetters direction="up">BLACK</FadeInLetters>
+          </div>
+        </h1>
+        <FadeIn delay={0.6}>
+          <p className="mt-6 text-xl text-neutral-600">
+            Full stack developer, designer, and general clever clogs.
+          </p>
+        </FadeIn>
       </Container>
       <Clients />
-      <Testimonial
-        className="mt-2 sm:mt-32 lg:mt-40"
-        theme="dark"
-        client={{}}
-      >
-        Harmony of creativity and logic makes the difference; perfect your art, master your code.
+      <Testimonial className="mt-2 sm:mt-32 lg:mt-40" theme="dark" client={{}}>
+        Harmony of creativity and logic makes the difference; perfect your art,
+        master your code.
       </Testimonial>
-
 
       <Services />
       <Portfolio />
