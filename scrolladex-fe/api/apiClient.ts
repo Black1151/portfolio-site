727ce1995a698b8438ac8b5a4a6e508339eb21e7
Daniel Black <danblack9988@gmail.com>
2025-03-02 11:52:50 +0000

    deployment debugging

--- scrolladex-fe/api/apiClient.ts
+++ scrolladex-fe/api/apiClient.ts
@@ -67,7 +67,7 @@ const handleFormData = async (request: any) => {
 
 const apiClient = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_SERVER_ADDRESS,
-  baseURL: "https://scrolladex.danielblack-dev.co.uk",
+  baseURL: "https://scrolladex.danielblack-dev.co.uk/api",
 });
 
 apiClient.interceptors.request.use(handleFormData);
